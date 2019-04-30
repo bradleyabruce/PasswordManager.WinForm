b094288e9768540dac474aaf0fab40b1ddefac48
bruceba <bruceba@mail.uc.edu>
2019-04-30 01:55:34 -0400

    Added functionality to login to user account on server

--- PasswordManager.WindowsApp/PasswordManager.WindowsApp/DAO/Utils.cs
+++ PasswordManager.WindowsApp/PasswordManager.WindowsApp/DAO/Utils.cs
@@ -71,5 +71,58 @@ namespace PasswordManager.WindowsApp.DAO
 
 
 
+
+        //connect to database with login
+        public bool loginToDatabase(string inputUsername, string inputPassword)
+        {
+            //store userID
+            string userID = "";
+
+            //query to pass to database
+            string queryString = "SELECT [UserID],[UserLoginEmail],[UserLoginPassword] FROM[PasswordManager].[dbo].[tUsers] WHERE UserLoginEmail = @user AND UserLoginPassword = @password";
+            
+            //pass values to sql server
+            using (SqlConnection conn = new SqlConnection(getConnectionString()))
+            {
+
+                SqlCommand command = new SqlCommand(queryString, conn);
+                command.Parameters.AddWithValue("@user", inputUsername);
+                command.Parameters.AddWithValue("@password", inputPassword);
+                conn.Open();
+
+                //query database
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+
+                    //return the userID of the username and password conbination
+                    while (reader.Read())
+                    {
+                        userID = (String.Format("{0}, {1}", reader["UserID"], reader["UserLoginPassword"]));     
+
+                    }
+
+                    //if nothing is returned, then there must not be a match and the username / password does not exist
+                    if (userID == "" || userID == null)
+                    {
+                        return false;
+                    }
+
+                }
+
+                //something went wrong
+                catch(Exception e)
+                {
+                    return false;
+                }
+                
+            }
+
+            //everything went well
+            return true;
+        }
+
+
+
     }
 }
--- PasswordManager.WindowsApp/PasswordManager.WindowsApp/Form1.cs
+++ PasswordManager.WindowsApp/PasswordManager.WindowsApp/Form1.cs
@@ -10,10 +10,12 @@ namespace PasswordManager.WindowsApp
 
         Utils utilClass = new Utils();
 
+
+        //on start up of the application
         public Form1()
         {
 
-            //set database status for user
+            //set database status for database
 
             InitializeComponent();
             if (utilClass.databaseCheck() == true)
@@ -25,20 +27,41 @@ namespace PasswordManager.WindowsApp
             {
                 lblStatus.Text = " Offline";
                 lblStatus.ForeColor = System.Drawing.Color.Red;
+                btnLogin.Enabled = false;
             }
 
         }
 
 
+        //on login button press
         private void BtnLogin_Click(object sender, EventArgs e)
         {
 
+            //get username and password from textbox
             string username = tbUsername.Text;
             string password = tbPassword.Text;
 
-           
+            //check to see if username or password is null and let user know
+            if (username == "" || password == "")
+            {
+                MessageBox.Show("Username and Password Required");
+            }
 
+            else
+            {
 
+                //if the login returns true alert the user
+                if (utilClass.loginToDatabase(username, password) == true)
+                {
+                    MessageBox.Show("Correct");
+                }
+
+                //if the login returns false alert the user 
+                else
+                {
+                    MessageBox.Show("Incorrect Login");
+                }
+            }
 
         }
     }
